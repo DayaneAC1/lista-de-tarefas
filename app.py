73b93da5223950a72fe72d7403a97c000e627b6c
DayaneAC1 <dayanealvescox@gmail.com>
2025-03-28 17:19:07 -0300

    Add files via upload
    Aula 28/03/25

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template # Importando a biblioteca flask
+# Importando a biblioteca flask
+from flask import Flask, render_template, request
+# Biblioteca para segurança no login
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import database
 
 app = Flask(__name__) # Criando um objeto do Flask chamado app
 
@@ -10,9 +15,19 @@ def hello():
 def login():
     return render_template('login.html')
 
-@app.route('/cadastro')
+#GET serve para "pegar" as informações de uma página
+#POST serve para enviar informações
+
+@app.route('/cadastro', methods=["GET", "POST"])
 def cadastro():
-    return render_template('cadastro.html')
+    if request.method == "POST":
+        form = request.form
+        if database.criar_usuario(form) == True:
+            return render_template('login.html')
+        else:
+            return "Ocorreu um erro ao cadastrar usuário"
+    else:    
+        return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -14,6 +14,23 @@ def criar_tabelas():
                    (id integer primary key, conteudo text, esta_concluida integer, email_usuario text, foreign key (email_usuario) references usuarios(email))''')
     
     conexao.commit()
+    
+def criar_usuario (formulario):
+    # Verificar se o email já existe no Banco de Dados
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT COUNT (email) FROM usuarios WHERE email=?''',(formulario['email'],))
+    conexao.commit()
+    
+    quantidade_de_emails_cadastrados = cursor.fetchone()
+    +
+    if(quantidade_de_emails_cadastrados[0] > 0):
+        print("LOG: Já existe esse e-mail cadastrado no banco!")
+        return False 
+    
+    cursor.execute('''INSERT INTO usuarios (email, nome, senha) VALUES (?, ?, ?)''', (formulario ['email'], formulario['nome'], formulario ['senha']))
+    conexao.commit()
+    return True
 
 if __name__ == '__main__':
     conectar_banco()
